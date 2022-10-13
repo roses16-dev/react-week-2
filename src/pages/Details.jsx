74f8cc7feb67fa6952d4f2b283e1cd350ab3aa10
Kim Minjoo <roses16.dev@gmail.com>
2022-10-13 19:45:07 +0900

    ; 추가

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 
 export default function Details () {
 
-    const id = useParams().id
+    const id = useParams().id;
 
     return <>
             <Header />
