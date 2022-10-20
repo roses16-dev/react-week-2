76822cb78441d7f7b29337db65c45bf083f43847
Kim Minjoo <roses16.dev@gmail.com>
2022-10-20 16:09:43 +0900

    test/devtools 출력 테스트

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,13 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import thunk from 'redux-thunk';
+
 import todoList from '../modules/todoList'
 
 const rootReducer = combineReducers({
     todoList:todoList
 
-})
+}, composeWithDevTools(applyMiddleware(thunk)))
 const store = createStore(rootReducer)
 
 export default store
